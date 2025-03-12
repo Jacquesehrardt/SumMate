4799be45b1827714a87625fa3fb8a731d0ae373e
Jacques Ehrardt <ehrardt38@gmail.com>
2025-03-12 15:32:56 -0300

    Added Shadcn

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,8 +9,20 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+const eslintConfig = {
+  ...compat.extends({
+    extends:[
+      "next/core-web-vitals",
+      "next/typescript",
+      "prettier",
+      "plugin:tailwindcss/recommended",
+    ],
+    plugins: ["prettier"],
+    reles: {
+      "prettier/prettier": "error",
+      "react/no-escape-entities": "off",
+    },
+  }),
+};
 
 export default eslintConfig;
