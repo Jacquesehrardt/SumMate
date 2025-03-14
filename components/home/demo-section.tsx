07328192b8137550844c592c2c9b46d5af6bb7f5
Jacques Ehrardt <ehrardt38@gmail.com>
2025-03-14 11:34:32 -0300

    Added How It Works Section

--- components/home/demo-section.tsx
+++ components/home/demo-section.tsx
@@ -1,11 +1,12 @@
 import { PizzaIcon } from "lucide-react";
+import BgGradientLandingPage from "@/components/home/bg-gradient-landing-page";
 
 export default function DemoSection() {
    return (
       <section className="relative">
          <div className="py-12 lg:py-24 mx-w-5xl mx-auto px-4 sm:px-6 lg:px-8 lg:pt-12">
             {/* Gradient */}
-            <div
+            {/* <div
                aria-hidden="true"
                className="pointer-events-none absolute inset-0 -z-10 transform-gpu overflow-hidden blur-3xl"
             >
@@ -16,7 +17,8 @@ export default function DemoSection() {
                   }}
                   className="relative left-[calc(50%-3rem)] aspect-1155/678 w-[36.125rem] -translate-x-1/2 bg-linear-to-br from-emerald-500 via-teal-500 to-cyan-500 opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                />
-            </div>
+            </div> */}
+            <BgGradientLandingPage />
             {/* Pizza Icon and text */}
             <div className="flex flex-col items-center text-center space-y-4">
                <div className="inline-flex items-center justify-center p-2 rounded-2xl bg-gray-100/80 backdrop-blur-xs border border-gray-500/20 mb-4">
