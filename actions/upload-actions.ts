c5df9f103412993071fee5ed45736a51d400dba3
Jacques Ehrardt <ehrardt38@gmail.com>
2025-03-28 14:53:18 -0300

    Added OpenAi and GeminiAI

--- actions/upload-actions.ts
+++ actions/upload-actions.ts
@@ -1,6 +1,8 @@
 "use server";
 
 import { fetchAndExtractPdfText } from "@/lib/langchain";
+import { generateSummaryFromOpenAi } from "@/lib/openai";
+import { generateSummaryFromGemini } from "@/lib/geminiai";
 
 export async function generatePdfSummary(uploadResponse: [{
    serverData: {
@@ -37,6 +39,40 @@ export async function generatePdfSummary(uploadResponse: [{
    try{
       const pdfText = await fetchAndExtractPdfText(pdfUrl);
       console.log({ pdfText })
+
+      let summary;
+      try{
+         summary = await generateSummaryFromOpenAi(pdfText);
+         console.log({ summary })
+      } catch(error){
+         console.log(error)
+         //Gemini
+         if(error instanceof Error && error.message === "RATE_LIMIT_EXCEEDED"){
+            try{
+               summary = await generateSummaryFromGemini(pdfText);
+            } catch(geminiError){
+               console.error("Gemini API failed after OpenAI quote exceeded", geminiError);
+               throw new Error("Failed to generate summary with available AI providers");
+            }
+         }
+      }
+
+      if(!summary){
+         return {
+            success: false,
+            message: "Failed to generate summary",
+            data: null,
+         };
+      }
+
+      return {
+         success: true,
+         message: "Summary generated successfully",
+         data: {
+            summary,
+         },
+      };
+
    } catch(err) {
       return {
          success: false,
--- components/upload/upload-form.tsx
+++ components/upload/upload-form.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { useUploadThing } from "@/utils/uploadthing";
-import UploadFormInput from "./upload-form-input";
+import UploadFormInput from "@/components/upload/upload-form-input";
 import { z } from "zod";
 import { toast } from "sonner"
 import { generatePdfSummary } from "@/actions/upload-actions";
+import { useRef, useState } from "react";
 
 const schema = z.object({
    file: z
@@ -14,6 +15,9 @@ const schema = z.object({
 });
 
 export default function UploadForm() {
+   const formRef = useRef<HTMLFormElement>(null);
+   const [ isLoading, setIsLoading ] = useState(false);
+
    const { startUpload, routeConfig } = useUploadThing("pdfUploader", {
       onClientUploadComplete: () => {
          console.log("uploaded successfully!");
@@ -31,49 +35,68 @@ export default function UploadForm() {
 
    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>)=> {
       e.preventDefault();
-      const formData = new FormData(e.currentTarget);
-      const file = formData.get("file") as File;
 
-      //Validating the fields
-      const validatedFields = schema.safeParse({ file });
-      console.log(validatedFields);
-      
-      if(!validatedFields.success) {
-         toast.error("Something went wrong", {
-            description:
-               validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file",
+      try{
+         setIsLoading(true);
+         const formData = new FormData(e.currentTarget);
+         const file = formData.get("file") as File;
+   
+         //Validating the fields
+         const validatedFields = schema.safeParse({ file });
+         console.log(validatedFields);
+         
+         if(!validatedFields.success) {
+            toast.error("Something went wrong", {
+               description:
+                  validatedFields.error.flatten().fieldErrors.file?.[0] ?? "Invalid file",
+               }
+            );
+            setIsLoading(false);
+            return;
+         }
+   
+         toast.info("Uploading PDF", {
+            description: "We are uploading your PDF!",
+         });
+   
+         //
+         const resp = await startUpload([file]);
+         if (!resp) {
+            toast.error("Something went wrong", {
+               description: "Please use a different file",
+            });
+            setIsLoading(false);
+            return;
+         }
+   
+         toast.info("Processing PDF", {
+            description: "Hang tight! Our AI is reading through your document!",
+         });
+   
+         //Parse the pdf using langchain
+         const result = await generatePdfSummary(resp);
+   
+         const { data = null, message = null} = result || {};
+         
+         if(data){
+            toast.info("Saving PDF", {
+               description: "Hang tight! We are saving your summary!",
+            });
+            formRef.current?.reset();
+            if(data.summary){
+               
             }
-         );
-         return;
+         };
+      } catch(error){
+         setIsLoading(false);
+         console.error("Error occurred", error);
+         formRef.current?.reset();
       }
-
-      toast.info("Uploading PDF", {
-         description: "We are uploading your PDF!",
-      })
-
-      //
-      const resp = await startUpload([file]);
-      if (!resp) {
-         toast.error("Something went wrong", {
-            description: "Please use a different file",
-         })
-         return;
-      }
-
-      toast.info("Processing PDF", {
-         description: "Hang tight! Our AI is reading through your document!",
-      })
-
-      //Parse the pdf using langchain
-      const summary = await generatePdfSummary(resp);
-      console.log({ summary })
-
-
    };
 
    return (
       <div className="flex felx-col justify-center gap-8 w-full max-w-2xl mx-auto">
-         <UploadFormInput onSubmit={handleSubmit} />
+         <UploadFormInput isLoading={isLoading} ref={formRef} onSubmit={handleSubmit} />
       </div>
    );
 }
